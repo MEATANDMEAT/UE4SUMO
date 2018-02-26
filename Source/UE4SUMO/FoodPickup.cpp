7c3c73a3513f460a516f381521c144cc202b7d33
Sultan Iljasov <BlackDragon038@hotmail.com>
2018-02-26 21:52:05 +0100

    Just turned off Tick ofr FoodPickup class, since its not needed here.

--- Source/UE4SUMO/FoodPickup.cpp
+++ Source/UE4SUMO/FoodPickup.cpp
@@ -7,7 +7,7 @@
 AFoodPickup::AFoodPickup()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 	FoodRoot = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
 	RootComponent = FoodRoot;
 	FoodMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FoodMesh"));
